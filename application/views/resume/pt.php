50a4a5539080b8078d8b5372b8392643d036f645
DiovaneGabriel <diovane.gabriel@gmail.com>
2016-10-29 13:53:20 -0200

    refresh resume

--- application/views/resume/pt.php
+++ application/views/resume/pt.php
@@ -23,11 +23,12 @@
 				<div class="yui-u first name">
 					<h1>Diovane Barbieri Gabriel</h1>
 					<h2>Desenvolvedor</h2>
-
+<!-- 
 					<div class="contact-info">
 						<h3><a href="mailto:diovane.gabriel@gmail.com">diovane.gabriel@gmail.com</a></h3>
-						<h3>+55 51 9930-6792</h3>
-					</div><!--// .contact-info -->
+						<h3>+55 51 9149-3845</h3>
+					</div>
+					 -->
 				</div>
 			</div><!--// .yui-gc -->
 		</div><!--// hd -->
@@ -72,33 +73,56 @@
 
 					<div class="yui-gf">
 						<div class="yui-u first">
-							<h2>Conhecimentos Técnicos</h2>
+							<h2>Conhecimentos Primários</h2>
 						</div>
 						<div class="yui-u">
 							<ul class="skills">
 								<li>Oracle DB</li>
-								<li>Oracle Forms and Reports</li>
-								<li>MySql</li>
-								<li class="last">PHP</li>
+								<li class="last">Oracle Forms & Reports</li>
 							</ul>
 
 							<ul class="skills">
-								<li>Javascript*</li>
-								<li>HTML*</li>
+								<li>MySql</li>
+								<li class="last">PL/SQL</li>
+							</ul>
+
+							<ul class="skills">
 								<li>Java</li>
 								<li class="last">iReport</li>
 							</ul>
 
 							<ul class="skills">
-								<li>C/C++*</li>
-								<li>Pascal*</li>
-								<li>Windows</li>
-								<li class="last">Linux*</li>
+								<li>Android Studio</li>
+								<li class="last">PHP</li>
 							</ul>
-							<font size="1">*Conhecimentos básicos</font></br>
-							<div class="ps">Além dos conhecimentos técnicos possuo conhecimento de outros softwares essenciais, como Microsoft Office e Adobe Photoshop.</div>
 						</div>
 					</div><!--// .yui-gf-->
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Conhecimentos Secundários</h2>
+						</div>
+						<div class="yui-u">
+							<ul class="skills">
+								<li>HTML</li>
+								<li class="last">CSS</li>
+							</ul>
+
+							<ul class="skills">
+								<li>JavaScript</li>
+								<li class="last">JQuery</li>
+							</ul>
+
+							<ul class="skills">
+								<li>PostgreSql</li>
+								<li class="last">Pascal</li>
+							</ul>
+
+							<ul class="skills">
+								<li>C/C++</li>
+								<li class="last">Linux</li>
+							</ul>
+						</div>
+					</div><!--// .yui-gf-->	
 
 					<div class="yui-gf">
 	
@@ -110,38 +134,34 @@
 
 							<div class="job last">
 								<h2>Size Systems</h2>
-								<h3>Desenvolvedor</h3>
+								<h3>Diretor - Desenvolvedor</h3>
 								<h4>2010-2016</h4>
-								<p>Como cofundador atuei em vários projetos distintos, que utilizaram diversas linguagens de programação, tais como, PL/SQL, Java, Pascal, Javascript e PHP. Com isso tive também experiências em ferramentas como, Oracle Forms and Reports, iReport, AngularJS, Ionic, Delphi e Lazarus. Atuei também nas áreas de suporte ao cliente, treinamento e implantação de soluções desenvolvidas principalmente em Oracle Forms and Reports.</p>
+								<p>Como cofundador atuo em vários projetos distintos, que utilizam diversas linguagens de programação, 
+								tais como, PL/SQL, PHP, JavaScript, Java e Pascal. Com isso tive também experiências em ferramentas como, 
+								Android Studio, Oracle Forms & Reports, iReport, Delphi e Lazarus. Atuo também nas áreas de suporte ao 
+								cliente, treinamento e implantação de soluções próprias.</p>
 							</div>
 
 						</div><!--// .yui-u -->
 					</div><!--// .yui-gf -->
 
 
-					<div class="yui-gf last">
+					<div class="yui-gf ">
 						<div class="yui-u first">
 							<h2>Educação</h2>
 						</div>
 						<div class="yui-u">
 							<div class="job">
 								<h2>Universidade Feevale - Novo Hamburgo, Rio Grande do Sul</h2>
-								<h4>2015-2016</h4>
-								<h3>Sistemas de Informação, cursando 2º semestre</h3>
+								<h4>2015-2019</h4>
+								<h3>Sistemas de Informação, cursando 3º semestre</h3>
 							</div>
 						</div>
 						<div class="yui-u">
 							<div class="job">
-								<h2>CCAA - Sapiranga, Rio Grande do Sul</h2>
-								<h4>2015-2016</h4>
-								<h3>Inglês, cursando 2º semestre</h3>
-							</div>
-						</div>
-						<div class="yui-u">
-							<div class="job">
-								<h2>Centro Sinodal Duque de Caxias - Sapiranga, Rio Grande do Sul</h2>
-								<h4>2015</h4>
-								<h3>Desenvolvimento Móvel Multiplataforma com HTML 5</h3>
+								<h2>HarvardX - edX</h2>
+								<h4>2016</h4>
+								<h3>Introdução à ciência da computação, cursando</h3>
 							</div>
 						</div>
 						<div class="yui-u">
@@ -160,13 +180,59 @@
 						</div>
 					</div><!--// .yui-gf -->
 
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Idiomas</h2>
+						</div>
+						<div class="yui-u">
+
+								<div class="language">
+									<h2>Português</h2>
+									<p>Nativo</p>
+								</div>
+
+								<div class="language">
+									<h2>Inglês</h2>
+									<p>Intermediário</p>
+								</div>
+						</div>
+					</div><!--// .yui-gf -->
+
+					<div class="yui-gf last">
+						<div class="yui-u first">
+							<h2>Contato</h2>
+						</div>
+						<div class="yui-u">
+
+								<div class="contact">
+									<h2>Telefone</h2>
+									<p>+55 51 9149-3845</p>
+								</div>
+
+								<div class="contact">
+									<h2>E-mail</h2>
+									<p>
+										<a href="mailto:diovane.gabriel@gmail.com">diovane.gabriel@gmail.com</a>
+									</p>
+								</div>
+								<div class="contact">
+									<h2>Web Site</h2>
+									<p>
+										<a href="http://diovane.me/">diovane.me</a>
+									</p>
+								</div>
+						</div>
+					</div><!--// .yui-gf -->
+
 
 				</div><!--// .yui-b -->
 			</div><!--// yui-main -->
 		</div><!--// bd -->
 
 		<div id="ft">
-			<p>Diovane Barbieri Gabriel &mdash; <a href="mailto:diovane.gabriel@gmail.com">diovane.gabriel@gmail.com</a> &mdash; +55 51 9930-6792</p></br>
+			<!-- 
+			<p>Diovane Barbieri Gabriel &mdash; <a href="mailto:diovane.gabriel@gmail.com">diovane.gabriel@gmail.com</a> &mdash; +55 51 9149-3845</p></br>
+			 -->
 			<a class="print" href="#" onclick="window.print(); return true;">Imprimir PDF</a>
 		</div><!--// footer -->
 
